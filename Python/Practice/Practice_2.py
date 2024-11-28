262cf32b1bd22b38c1d3a0ba112c2e7d65f819af
Hasby201 <mhasbiash704201@gmail.com>
2024-11-28 08:53:42 +0700

    Practice_2.py

--- Python/Practice/Practice_2.py
+++ Python/Practice/Practice_2.py
@@ -1,3 +1,30 @@
 # Practice_2
-# Python_
+# Python_Numbers
 
+# Example_1
+# Int
+firstVar = 1
+secondVar = 100
+
+print(type(firstVar))
+print(secondVar)
+
+
+
+# Example_2
+# Float
+thirdVar = 0.1
+fourthVar = -1000.5
+
+print(type(thirdVar))
+print(fourthVar)
+
+
+
+# Example_3
+# Complex
+fifthVar = 5j
+sixthVar = 5+5j
+
+print(type(fifthVar))
+print(sixthVar)
